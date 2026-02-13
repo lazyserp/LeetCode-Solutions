f1c41fdd3ccbd4f023d69c71fb2a37e9fc71aecb
Aman <hazyberg@hotmail.com>
2026-02-13 22:18:29 +0530

    Time: 38 ms (27.31%) | Memory: 31.5 MB (39.03%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int LCS(string &s,string &t,int i,int j)
+    int LPS(string s)
     {
-        if ( i==0 || j==0) return 0;
-        if ( dp[i][j] != -1) return dp[i][j];
-
-        if ( s[i-1] == t[j-1]) return dp[i][j] = 1 + LCS(s,t,i-1,j-1);
-
-        return dp[i][j] = max(LCS(s,t,i-1,j),LCS(s,t,i,j-1));
-
-    }
-
-    int minInsertions(string s) 
-    {
+        int n = s.size();
         string t = s;
         reverse(t.begin(),t.end());
-        int n = s.size();
 
-        dp.assign(n+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
 
-        int LPSanswer = LCS(s,t,n,n);
-        return n - LPSanswer;
+        for(int i= 1 ; i <= n ; i++)
+        {
+            for(int j= 1 ; j <= n ; j++)
+            {
+                if ( s[i-1] == t[j-1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][n];
+    }
+    int minInsertions(string s) 
+    {
+        return s.size() - LPS(s);
         
     }
 };
