29ec089a5afd83faa760994c80a68b764e8d2236
Aman <hazyberg@hotmail.com>
2026-02-23 22:45:36 +0530

    Time: 10 ms (72.48%) | Memory: 20 MB (30.18%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        res = []
+        i = len(num)-1
+
+        while i >= 0 or k > 0:
+            if i >= 0:
+                k += num[i]
+
+            res.append(k % 10)
+            k = k // 10
+            i -= 1
+
+        return res[::-1]
+        
