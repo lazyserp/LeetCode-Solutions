d5c84319e6adbf627cd7ca6e335b8338d2a6e083
Aman <hazyberg@hotmail.com>
2026-03-07 01:29:07 +0530

    Time: 127 ms (44.82%) | Memory: 33.8 MB (50.56%) - LeetSync

---
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -0,0 +1,26 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        r = len(matrix)
+        c = len(matrix[0])
+
+        self.dp = [ [0] * (c+1) for i in range(0,r+1)]
+
+        for i in range(1,r+1):
+            for j in range(1,c+1):
+                self.dp[i][j] = matrix[i-1][j-1] + self.dp[i-1][j] + self.dp[i][j-1] - self.dp[i-1][j-1]
+        
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        row1 += 1
+        col1 += 1
+        row2 += 1
+        col2 += 1
+
+        return self.dp[row2][col2] - self.dp[row1-1][col2] - self.dp[row2][col1-1] + self.dp[row1-1][col1-1]
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
