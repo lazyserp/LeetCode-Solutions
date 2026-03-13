8df6cecef7c256daa9d756b0e1c07e5594ca2b6d
Aman <hazyberg@hotmail.com>
2026-03-13 22:43:39 +0530

    Time: 191 ms (11.84%) | Memory: 19.4 MB (68.85%) - LeetSync

--- 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.py
@@ -3,13 +3,18 @@ class Solution:
         ans = 0
         n = len(nums)
 
+        pfx = [0] *( n+1)
+        pfx[0] = nums[0]
+        for i in range(1,n):
+            pfx[i] = pfx[i-1] + nums[i]
+
         for i in range(0,n - firstLen+1):
-            firstSum = sum(nums[i:i+firstLen])
+            firstSum = pfx[i+firstLen-1] - pfx[i-1]
 
             for j in range(0,n-secondLen+1):
                 
                 if i + firstLen <= j or j + secondLen <= i:
-                    secondSum = sum(nums[j:j+secondLen])    
+                    secondSum = pfx[j+secondLen-1] - pfx[j-1]   
                     ans = max(ans , firstSum+secondSum)
 
         return ans
