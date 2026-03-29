1771085f31ef57638252672d8ef305d1cb6cf231
Aman <hazyberg@hotmail.com>
2026-03-29 14:53:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (72.40%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -4,17 +4,15 @@ class Solution:
             return 0
         if n == 1 or n == 2:
             return 1
+            
+        dp = [0] * (n+1)
 
-        a = 0
-        b = 1
-        c = 1
-        
+        dp[0] , dp[1] ,dp[2] = 0,1,1
+
         for i in range(3,n+1):
-            a,b,c = b , c ,a+b+c
-        
-        return c
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
-       
+        return dp[n]
 
 
 
