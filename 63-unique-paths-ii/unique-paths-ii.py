35eff9cc75c954ed3e4b9db72f36636d627b2e48
Aman <hazyberg@hotmail.com>
2026-03-31 18:00:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (77.98%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,38 +1,34 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
+        rows = len(obstacleGrid)
+        cols = len(obstacleGrid[0])
 
-        dp = [ [0] * (n) for i in range(m+1) ]
+        dp = [ [0 for i in range(cols)] for j in range(rows)]
 
-        if ( obstacleGrid[m-1][n-1] == 1):
-            dp[m-1][n-1] = 0
+        if obstacleGrid[rows-1][cols-1] == 1:
             return 0
-        else:
-            dp[m-1][n-1] = 1
+        dp[rows-1][cols-1] = 1
 
-        for i in range(n-2,-1,-1):
-            if obstacleGrid[m-1][i] == 1 or dp[m-1][i+1] == 0:
-                dp[m-1][i] = 0
-            else:
-                dp[m-1][i] = 1
-        
-        for j in range(m-2,-1,-1):
-            if obstacleGrid[j][n-1] == 1 or dp[j+1][n-1]  == 0:
-                dp[j][n-1]  = 0
-            else:
-                dp[j][n-1]  = 1
 
-        for i in range(m-2,-1,-1):
-            for j in range(n-2,-1,-1):
+        for i in range(cols-2,-1,-1):
+            if obstacleGrid[rows-1][i] == 1:
+                dp[rows-1][i] = 0
+            else:
+                dp[rows-1][i] = dp[rows-1][i+1]
+
+
+        for i in range(rows-2,-1,-1):
+            if obstacleGrid[i][cols-1] == 1:
+                dp[i][cols-1] = 0
+            else:
+                dp[i][cols-1] = dp[i+1][cols-1]
+
+
+        for i in range(rows-2,-1,-1):
+            for j in range(cols-2,-1,-1):
                 if obstacleGrid[i][j] == 1:
                     dp[i][j] = 0
                 else:
                     dp[i][j] = dp[i+1][j] + dp[i][j+1]
         
         return dp[0][0]
-        
-
-        
-
-        
