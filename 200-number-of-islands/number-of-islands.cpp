52a19b0e554ec67801371ecef445b37b7abc5dd2
Aman <hazyberg@hotmail.com>
2026-04-25 20:36:47 +0530

    Time: 23 ms (84.04%) | Memory: 17.5 MB (41.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
-    int m ;
-    int n ;
-
-    void BFS(int i,int j, vector<vector<char>>& grid ,  vector<vector<int>> &visited)
+    void dfs(int i, int j, vector<vector<int>> &visited, vector<vector<char>> &grid)
     {
-        if ( i < 0 || i >= m) return ;
-        if ( j < 0 || j >= n) return ;
-        if ( grid[i][j] == '0') return ;
+        int n = grid.size();
+        int m = grid[0].size();
+        if ( i < 0 or j < 0) return ;   
+        if ( i >= n or j >= m) return ;
+
+        if (grid[i][j] == '0') return ;
         if (visited[i][j] == 1) return ;
+
+
 
         visited[i][j] = 1;
 
-        BFS(i+1,j,grid,visited);
-        BFS(i,j-1,grid,visited);
-        BFS(i-1,j,grid,visited);
-        BFS(i,j+1,grid,visited);
+        dfs(i+1,j,visited,grid);
+        dfs(i,j-1,visited,grid);
+        dfs(i-1,j,visited,grid);
+        dfs(i,j+1,visited,grid);
     }
+
     int numIslands(vector<vector<char>>& grid) 
     {
-        m = grid.size();
-        n = grid[0].size();
+        int numOfIslands = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,0));
 
-        vector<vector<int>> visited(m,vector<int>(n,0));
-
-        int ans = 0;
-
-        for(int i=0 ; i < m ;i++)
+        for(int i=0; i < n; i++)
         {
-            for(int j=0 ; j < n ;j++)
+            for(int j=0 ; j <m ;j++)
             {
-                if ( grid[i][j] == '1' && visited[i][j] == 0)
+                if ( grid[i][j] == '1' && visited[i][j] != 1)
                 {
-                    ans++;
-                    BFS(i,j,grid,visited);
+                    numOfIslands++;
+                    dfs(i,j,visited,grid);
                 }
             }
         }
 
-        return ans;
+        return numOfIslands;
+
 
 
         
