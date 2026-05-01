2663dc227828f9aa7045928b62dc103879cd99f1
Aman <hazyberg@hotmail.com>
2026-05-01 23:45:42 +0530

    Time: 11 ms (21.55%) | Memory: 19.1 MB (90.80%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
     {
         vector<vector<int>> adj(numCourses);
-        vector<int> inDegree(numCourses);
+        vector<int> indeg(numCourses);
 
-        for(auto &i : prerequisites)
+        for(auto &p : prerequisites)
         {
-            adj[i[1]].push_back(i[0]);
-            inDegree[i[0]]++;
+            adj[p[1]].push_back(p[0]);
+            indeg[p[0]]++;
         }
 
         queue<int> q;
-        for(int i=0 ; i < numCourses ;i++)
+        for(int i=0 ; i <numCourses ;i++)
         {
-            if (inDegree[i] == 0)
-            {
-                q.push(i);
-            }
+            if ( indeg[i] == 0) q.push(i);
         }
 
         int visited = 0;
-        while (!q.empty())
+        while ( !q.empty())
         {
             int node = q.front();
             q.pop();
             visited++;
 
-            for (int nbr : adj[node])
+            for(int i : adj[node])
             {
-                inDegree[nbr] -= 1;
-                if (inDegree[nbr] == 0) q.push(nbr);
+                indeg[i]--;
+                if ( indeg[i] == 0) q.push(i);
             }
         }
 
-        if ( numCourses == visited) return true;
-        return false;
-
+        if (visited != numCourses) return false;
+        return true;
         
     }
 };
